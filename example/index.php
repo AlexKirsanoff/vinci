13c4463d62e7e7e767b61b054eae70371794dfc8
AlexKirsanoff <americc@yandex.ru>
2018-08-16 17:04:02 +0300

    update

--- example/index.php
+++ example/index.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Vinci\Vinci;
+use Vinci\{
+    Vinci,
+    Request
+};
 use GuzzleHttp\Exception\GuzzleException;
 
 $path = __DIR__ . '/../img/photo.jpg';
